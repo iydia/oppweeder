8f485f82d3d98991496133f2de9c6e879dad3173
iydia <lydia.he23@ycdsbk12.ca>
2023-07-09 14:58:33 -0400

    Someone Please Help

---
+++ Data.java
@@ -0,0 +1,12 @@
+
+public class Data {
+    private String username;
+
+    public Data(String theUsername){
+        this.username = theUsername;
+    }
+
+    public String toString(){
+        return this.username + "\n";
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ListSort followers = new ListSort();
+
+        System.out.println(followers.toString());
+    }
+}
