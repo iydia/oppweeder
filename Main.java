3fadc9ff6c055f5a29fc462316e029fefb2b41f3
Lydia He <75390984+iydia@users.noreply.github.com>
2024-05-19 20:27:50 -0400

    Done

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 class Main {
     public static void main(String[] args) {
-        List<String> followers = readCSV("Test Followers - Sheet1.csv");
-        List<String> following = readCSV("Test Following - Sheet1.csv");
+        List<String> followers = readCSV("Followers M24 - Sheet1.csv");
+        List<String> following = readCSV("Following M24 - Sheet1.csv");
         List<String> dawgs = new ArrayList<>();
         dawgs.addAll(followers);
         dawgs.retainAll(following);
@@ -21,9 +22,33 @@ class Main {
         //System.out.println(following);
         //System.out.println(dawgs);
         //System.out.println(disciples);
-        System.out.println(opps);
+        printList(filterUsernames(filterValidUsernames(opps)));
     }
 
+    // Filters out names with spaces
+    private static List<String> filterUsernames(List<String> usernames) {
+        List<String> filteredUsernames = new ArrayList<>();
+        for (String username : usernames) {
+            if (!username.contains(" ")) {
+                filteredUsernames.add(username);
+            }
+        }
+        return filteredUsernames;
+    }
+
+    // Filters out usernames that do not include a period, underscore, number, or alphabet
+    private static List<String> filterValidUsernames(List<String> usernames) {
+        List<String> filteredUsernames = new ArrayList<>();
+        Pattern pattern = Pattern.compile(".*[._\\dA-Za-z].*");
+        for (String username : usernames) {
+            if (pattern.matcher(username).matches()) {
+                filteredUsernames.add(username);
+            }
+        }
+        return filteredUsernames;
+    }
+
+    // CSV scraper
     public static List<String> readCSV(String fileName){
         List<String> data = new ArrayList<>();
         try (BufferedReader file = new BufferedReader(new FileReader(fileName))){
@@ -36,4 +61,12 @@ class Main {
         }
         return data;
     }
+
+    // Prints individual entries in newlines
+    private static void printList(List<String> list) {
+        for (String element : list) {
+            System.out.println(element);
+        }
+    }
+
 }
