01472b62b17d6e7d2aeba77a45ff8c88edbd963e
iydia <hly.liya@gmail.com>
2025-04-05 15:54:13 -0400

    Using lombok

--- src/main/java/oppweeder/config/InstagramProperties.java
+++ src/main/java/oppweeder/config/InstagramProperties.java
@@ -2,7 +2,11 @@ package oppweeder.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString(exclude = "password")
 @Component
 @ConfigurationProperties(prefix = "application.instagram")
 public class InstagramProperties {
